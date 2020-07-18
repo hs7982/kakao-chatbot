d5cff692c0e27033971a0eca1df410a548bfb29b
unknown <hs6247982@gmail.com>
2020-07-18 15:41:41 +0900

    bug fix

--- crawl.py
+++ crawl.py
@@ -8,7 +8,8 @@ import logging
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter('%(asctime)s %(name)s - [%(levelname)s]: %(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s %(name)s - [%(levelname)s]: %(message)s')
 
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
@@ -20,28 +21,30 @@ log.addHandler(file_handler)
 
 pw = passwd.Passwd()
 
-#학교 기본정보 코드
+# 학교 기본정보 코드
 eduCode = 'pen.go.kr'
 schoolCode = 'C100000478'
 
-#중식, 석식 코드
+# 중식, 석식 코드
 lunch = '2'
 dinner = '3'
+
 
 def mysql_del():
     try:
         # db연결
-        conn = pymysql.connect(host="localhost", user="root", passwd=pw.passwd, db="jungang_meal", charset='utf8')
-        #curs생성
+        conn = pymysql.connect(host="localhost", user="root",
+                               passwd=pw.passwd, db="jungang_meal", charset='utf8')
+        # curs생성
         curs = conn.cursor()
     except:
         log.error("DB서버 연결에 실패하였습니다.")
-    #데티어 비우기
+    # 데티어 비우기
     try:
-        sql=("""TRUNCATE `week_meal_2`""")
+        sql = ("""TRUNCATE `week_meal_2`""")
         curs.execute(sql)
         conn.commit()
-        sql=("""TRUNCATE `week_meal_3`""")
+        sql = ("""TRUNCATE `week_meal_3`""")
         curs.execute(sql)
         conn.commit()
         conn.close()
@@ -49,46 +52,55 @@ def mysql_del():
     except:
         log.error("DB서버에서 기존 식단 데이터를 지우는 중 오류가 발생하였습니다.")
 
+
 def mysql_in(day, meal, when):
     log.info("DB서버에 저장중입니다. 종류: "+when+", 요일: "+str(day))
     # db연결
-    conn = pymysql.connect(host="localhost", user="root", passwd=pw.passwd, db="jungang_meal", charset='utf8')
+    conn = pymysql.connect(host="localhost", user="root",
+                           passwd=pw.passwd, db="jungang_meal", charset='utf8')
     day = str(day)
-    #curs생성
+    # curs생성
     curs = conn.cursor()
 
-    #데티어 생성
-    sql=("""INSERT INTO week_meal_"""+when+"""(day, meal) VALUES ('"""+ day + "', '" + meal + """')""")
+    # 데티어 생성
+    sql = ("""INSERT INTO week_meal_"""+when +
+           """(day, meal) VALUES ('""" + day + "', '" + meal + """')""")
     curs.execute(sql)
     conn.commit()
     conn.close()
 
+
 def nies_parser(eduCode, schoolCode, when):
     now = datetime.datetime.now()
     log.info("NEIS에서 주간 급식정보를 가져옵니다. 종류: "+when)
-    url = 'http://stu.'+ eduCode +'/sts_sci_md01_001.do?schulCode='+ schoolCode +'&schulCrseScCode=4&schMmealScCode='+ when +'&schYmd='+now.strftime('%Y.%m.%d')
+    url = 'http://stu.' + eduCode + '/sts_sci_md01_001.do?schulCode=' + schoolCode + \
+        '&schulCrseScCode=4&schMmealScCode=' + \
+        when + '&schYmd='+now.strftime('%Y.%m.%d')
     log.debug(url)
     req = urllib.request.urlopen(url, timeout=3)
     soup = bs(req, 'html.parser')
     log.info("데이터 처리중... 종류: "+when)
-    meal = soup.find_all('td', {'class':'textC'})[7:14]
-    
-    #0=일, 1=월, 2=화 ... 6=토
+    meal = soup.find_all('td', {'class': 'textC'})[7:14]
+
+    # 0=일, 1=월, 2=화 ... 6=토
     today = 0
     while today < 7:
         menu = str(meal[today])
-        menu = menu.replace('<td class="textC">', '').replace('</td>', '').replace('<br/>', '\n').replace('(중앙)','').replace('&amp;','&').replace('(중앙','').replace('(증앙)','').replace('<td class="textC last">','')
+        menu = menu.replace('<td class="textC">', '').replace('</td>', '').replace('<br/>', '\n').replace(
+            '(중앙)', '').replace('&amp;', '&').replace('(중앙', '').replace('(증앙)', '').replace('<td class="textC last">', '')
         for n in range(18, 0, -1):
             menu = menu.replace(str(n)+".", "")
-        
-        #정보가 없을경우
+
+        # 정보가 없을경우
         if menu == '' or menu == ' ':
             menu = '급식 정보가 없습니다.\n'
-        
-        #db로 저장
+
+        # db로 저장
         mysql_in(today, menu, when)
 
         today = today+1
+
+
 log.info("NEIS 급식 크롤러를 시작합니다.")
 mysql_del()
 nies_parser(eduCode, schoolCode, lunch)
--- main.py
+++ main.py
@@ -7,25 +7,27 @@ application = Flask(__name__)
 
 pw = passwd.Passwd()
 
+
 def mysql_conn():
     try:
         global conn, curs
         # db연결
         conn = pymysql.connect(
-            host="localhost", 
-            user="root", 
-            passwd=pw.passwd, 
-            db="jungang_meal", 
+            host="localhost",
+            user="root",
+            passwd=pw.passwd,
+            db="jungang_meal",
             charset='utf8')
-        #curs생성
+        # curs생성
         curs = conn.cursor()
     except:
         print("db연결 중 오류가 발생했습니다. mysql_del")
 
+
 def mysql_lunch(day):
-    #중식
+    # 중식
     mysql_conn()
-    sql=("""SELECT meal FROM `week_meal_2`""")
+    sql = ("""SELECT meal FROM `week_meal_2`""")
     curs.execute(sql)
     n = 0
     while n < day+1:
@@ -36,10 +38,11 @@ def mysql_lunch(day):
 
     return lunch
 
+
 def mysql_dinner(day):
-    #석식
+    # 석식
     mysql_conn()
-    sql=("""SELECT meal FROM `week_meal_3`""")
+    sql = ("""SELECT meal FROM `week_meal_3`""")
     curs.execute(sql)
     n = 0
     while n < day+1:
@@ -50,10 +53,11 @@ def mysql_dinner(day):
 
     return dinner
 
+
 def mysql_sc(month):
-    #학사일정
+    # 학사일정
     mysql_conn()
-    sql=("""SELECT * FROM `sc_schedule` WHERE `month` LIKE '%s' ORDER BY `month` DESC""" %month)
+    sql = ("""SELECT * FROM `sc_schedule` WHERE `month` LIKE '%s' ORDER BY `month` DESC""" % month)
     try:
         curs.execute(sql)
         schedule = curs.fetchone()
@@ -61,14 +65,16 @@ def mysql_sc(month):
         conn.commit
         conn.close()
     except:
-        schedule = "%s: 해당월의 정보가 없습니다.\n학년도 시작 이후 정보가 업데이트 될 예정입니다."%month
-    
+        schedule = "%s: 해당월의 정보가 없습니다.\n학년도 시작 이후 정보가 업데이트 될 예정입니다." % month
+
     return schedule
+
 
 @application.route('/')
 def Home():
     page = "Algorithm Factory api server"
     return page
+
 
 @application.route('/log')
 def log():
@@ -76,6 +82,7 @@ def log():
     logr = logf.read()
     logf.close()
     return logr
+
 
 @application.route('/schedule', methods=['POST'])
 def schedule():
@@ -88,21 +95,21 @@ def schedule():
     dt = datetime.datetime.now()
 
     if scm_data == "이번달":
-        month=dt.strftime("%y")+str(dt.month)
+        month = dt.strftime("%y")+str(dt.month)
         schedule = mysql_sc(month)
 
         msg = dt.strftime("%Y년 %m월")+"의 학사일정 입니다."
 
         dataSend = {
-            "version" : "2.0",
-            "data" : {
-                "title" : msg,
-                "content" : schedule,
+            "version": "2.0",
+            "data": {
+                "title": msg,
+                "content": schedule,
             }
         }
-    
+
     elif scm_data == "다음달":
-        month=dt.month+1
+        month = dt.month+1
         if month < 13:
             month = dt.strftime("%y")+str(month)
         else:
@@ -110,17 +117,17 @@ def schedule():
             month = year[2:]+str(month-12)
         schedule = mysql_sc(month)
 
-        msg = "20%s년 %s월의 학사일정 입니다."%(month[:2],month[2:])
+        msg = "20%s년 %s월의 학사일정 입니다." % (month[:2], month[2:])
 
         dataSend = {
-            "version" : "2.0",
-            "data" : {
-                "title" : msg,
-                "content" : schedule,
+            "version": "2.0",
+            "data": {
+                "title": msg,
+                "content": schedule,
             }
         }
 
-    #사용자가 월을 직접 입력하는 경우
+    # 사용자가 월을 직접 입력하는 경우
     elif scm_data[0] == "n":
         today_mon = dt.month
         today_y = dt.year
@@ -128,7 +135,7 @@ def schedule():
 
         if mon_data == "1월" or mon_data == "2월":
             # 한 해의 학년도는 2월에 종료됨
-            #ex: 2019년 12월에 2월 정보를 요청 -> 19년 2월이 아닌 2020년 2월 정보 제공
+            # ex: 2019년 12월에 2월 정보를 요청 -> 19년 2월이 아닌 2020년 2월 정보 제공
 
             if today_mon > 2:
                 year = str(today_y+1)
@@ -136,36 +143,37 @@ def schedule():
             else:
                 year = str(today_y-1)
                 month = year[2:]+mon_data[0:-1]
-        
-        #1,2월에 3~12월 요청시 해당 학년도의 정보를 제공하기 위함
-        #ex: 2020년 1월에 11월 정보 요청 -> 2019년 11월 정보 제공
+
+        # 1,2월에 3~12월 요청시 해당 학년도의 정보를 제공하기 위함
+        # ex: 2020년 1월에 11월 정보 요청 -> 2019년 11월 정보 제공
         elif today_mon > 2:
             month = dt.strftime("%y")+mon_data[0:-1]
         else:
             year = str(today_y-1)
             month = year[2:]+mon_data[0:-1]
 
-        msg = "20%s년 %s월의 학사일정 입니다."%(month[:2], mon_data[0:-1])
+        msg = "20%s년 %s월의 학사일정 입니다." % (month[:2], mon_data[0:-1])
         schedule = mysql_sc(month)
 
         dataSend = {
-            "version" : "2.0",
-            "data" : {
-                "title" : msg,
-                "content" : schedule,
+            "version": "2.0",
+            "data": {
+                "title": msg,
+                "content": schedule,
             }
         }
 
     else:
         dataSend = {
-            "version" : "2.0",
-            "data" : {
-                "title" : "죄송합니다.",
-                "content" : "오류가 발생하였습니다. 잠시 후 다시 시도하거나, 관리자에게 문의해주세요.",
+            "version": "2.0",
+            "data": {
+                "title": "죄송합니다.",
+                "content": "오류가 발생하였습니다. 잠시 후 다시 시도하거나, 관리자에게 문의해주세요.",
             }
         }
 
     return jsonify(dataSend)
+
 
 @application.route('/meal', methods=['POST'])
 def Meal():
@@ -185,12 +193,13 @@ def Meal():
         else:
             lunch = mysql_lunch(day+1)
             dinner = mysql_dinner(day+1)
-        msg = "[중식]\n" + str(lunch[0]) + "\n[석식]\n" + str(dinner[0])+"\n※위 식단은 학교나 시장 사정에 의하여 변경될 수 있습니다."
+        msg = "[중식]\n" + str(lunch[0]) + "\n[석식]\n" + \
+            str(dinner[0])+"\n※위 식단은 학교나 시장 사정에 의하여 변경될 수 있습니다."
         dataSend = {
-            "version" : "2.0",
-            "data" : {
-                "title" : "오늘의 식단입니다.",
-                "content" : msg,
+            "version": "2.0",
+            "data": {
+                "title": "오늘의 식단입니다.",
+                "content": msg,
             }
         }
 
@@ -206,17 +215,18 @@ def Meal():
             lunch = mysql_lunch(day+2)
             dinner = mysql_dinner(day+2)
 
-        msg = "[중식]\n" + str(lunch[0]) + "\n[석식]\n" + str(dinner[0])+"\n※위 식단은 학교나 시장 사정에 의하여 변경될 수 있습니다."
+        msg = "[중식]\n" + str(lunch[0]) + "\n[석식]\n" + \
+            str(dinner[0])+"\n※위 식단은 학교나 시장 사정에 의하여 변경될 수 있습니다."
         dataSend = {
-            "version" : "2.0",
-            "data" : {
-                "title" : "내일의 식단입니다.",
-                "content" : msg,
+            "version": "2.0",
+            "data": {
+                "title": "내일의 식단입니다.",
+                "content": msg,
             }
         }
-    
-    #요일을 직접 입력하는 경우
-    elif day_data[1:] == "요일":        
+
+    # 요일을 직접 입력하는 경우
+    elif day_data[1:] == "요일":
         if day_data == "월요일":
             sel_day = 1
             lunch = mysql_lunch(sel_day)
@@ -241,7 +251,7 @@ def Meal():
             sel_day = 5
             lunch = mysql_lunch(sel_day)
             dinner = mysql_dinner(sel_day)
-        
+
         elif day_data == "토요일":
             sel_day = 6
             lunch = mysql_lunch(sel_day)
@@ -251,38 +261,41 @@ def Meal():
             sel_day = 0
             lunch = mysql_lunch(sel_day)
             dinner = mysql_dinner(sel_day)
-        
+
         else:
             print("Error")
 
-        msg = "[중식]\n" + str(lunch[0]) + "\n[석식]\n" + str(dinner[0])+"\n※위 식단은 학교나 시장 사정에 의하여 변경될 수 있습니다."
-        
+        msg = "[중식]\n" + str(lunch[0]) + "\n[석식]\n" + \
+            str(dinner[0])+"\n※위 식단은 학교나 시장 사정에 의하여 변경될 수 있습니다."
+
         day = dt.weekday()
 
-        #이미 지나간 요일일 경우 안내
-        #토요일에 월요일의 정보를 요청 -> 이미 제공된 급식 정보임을 안내
-        if day+1 > sel_day:
-            notice = "[안내] 이미 제공된 " +day_data+ "의 식단을 표시중입니다.\n다음주의 식단 정보는 일요일에 업데이트되니 참고 바랍니다.\n\n"
+        # 이미 지나간 요일일 경우 안내
+        # 토요일에 월요일의 정보를 요청 -> 이미 제공된 급식 정보임을 안내
+        if day+1 > sel_day and day != 0:
+            notice = "[안내] 이미 제공된 " + day_data + \
+                "의 식단을 표시중입니다.\n다음주의 식단 정보는 일요일에 업데이트되니 참고 바랍니다.\n\n"
         else:
             notice = ""
 
         dataSend = {
-            "version" : "2.0",
-            "data" : {
-                "title" : day_data + "의 식단입니다.",
-                "content" : notice + msg,
+            "version": "2.0",
+            "data": {
+                "title": day_data + "의 식단입니다.",
+                "content": notice + msg,
             }
         }
 
     else:
         dataSend = {
-            "version" : "2.0",
-            "data" : {
-                "title" : "죄송합니다.",
-                "content" : "오류가 발생하였습니다. 잠시 후 다시 시도하거나, 관리자에게 문의해주세요.",
+            "version": "2.0",
+            "data": {
+                "title": "죄송합니다.",
+                "content": "오류가 발생하였습니다. 잠시 후 다시 시도하거나, 관리자에게 문의해주세요.",
             }
         }
     return jsonify(dataSend)
+
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
